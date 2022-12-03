47dc58e30d5346ae280c08c06db6e888d30cc63b
Moos <jimmemiddelkoop@gmail.com>
2022-12-03 12:45:22 +0100

    created template

---
+++ aoc-2022/template.py
@@ -0,0 +1,16 @@
+from aoc_helper import *
+
+# CHANGE DAY
+DAY = 4 
+
+def read_input(input):
+    pass
+
+def part1(input):
+    return solution
+
+def part2(input):
+    return solution
+
+run(f'inputs/{DAY}.txt', part1, part2)
+
